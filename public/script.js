91825cd04752354328582b9f597a666dfdca42f0
SeanCox <sean.cox@mac.com>
2016-10-22 12:40:48 -0700

    [feature] added orbit controls

--- public/script.js
+++ public/script.js
@@ -5,14 +5,45 @@ var renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
+window.addEventListener('resize', function(){
+  var width = window.innerWidth
+  var height = window.innerHeight
+  renderer.setSize(width, height)
+  camera.aspect = width / height
+  camera.updateProjectionMatrix()
+})
+
+controls = new THREE.OrbitControls(camera, renderer.domElement)
+
+//create the shape
+var geometry = new THREE.BoxGeometry(1, 1, 1)
+var cubeMaterials = [
+  new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/1.jpg'), side: THREE.DoubleSide}),
+  new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/2.jpg'), side: THREE.DoubleSide}),
+  new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/3.jpg'), side: THREE.DoubleSide}),
+  new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/4.jpg'), side: THREE.DoubleSide}),
+  new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/5.jpg'), side: THREE.DoubleSide}),
+  new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/6.jpg'), side: THREE.DoubleSide}),
+]
+
+// greate a material, color or image texture
+var material = new THREE.MeshNormalMaterial({color: 0xffffff, wireframe: false})
+var cube = new THREE.Mesh( geometry, material)
+scene.add(cube)
+
+camera.position.z = 3;
+
+// game logic
 var update = function(){
-
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.005
 }
-
+// draw scene
 var render = function(){
   renderer.render(scene, camera)
 }
 
+// run game loop (update, render, repeat)
 var GameLoop = function(){
   requestAnimationFrame(GameLoop)
 
